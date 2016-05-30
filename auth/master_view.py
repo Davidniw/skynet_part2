8af6ebb9d18b5f570457cbc68bada02a6545ab7d
David N <David N>
2016-05-30 12:32:50 +1000

    Trying master_view file to work with master_sign

--- auth/master_view.py
+++ auth/master_view.py
@@ -1,12 +1,22 @@
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
 import os
 
 
 def decrypt_valuables(f):
+    #Check if a key pair exsits, if not, exit
+    if not os.path.exists(masterkey.pem):
+        print("No key pair has been initialised")
+        os.exit(1)
+    # Import key from file
+    key = RSA.importKey(open('masterkey.pem').read())
     # TODO: For Part 2, you'll need to decrypt the contents of this file
     # The existing scheme uploads in plaintext
     # As such, we just convert it back to ASCII and print it out
-    decoded_text = str(f, 'ascii')
+    decoded_text = key.decrypt(f)
     print(decoded_text)
+    return decoded_text
 
 
 if __name__ == "__main__":
